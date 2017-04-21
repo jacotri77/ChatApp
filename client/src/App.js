5467fd91e18f9b844478be39e502228629bd0a57
Erik Jacobsen <slowerthanyou@gmail.com>
2017-04-21 15:06:54 -0700

    added Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,21 @@
 import React, { Component } from 'react'
 import './index.css'
-import { addMessage } from './api/messaging'
-import {connect} from 'react-redux'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+// import Login from './login'
+import ChatRoom from './chatRoom'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      message: ''
-    }
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    addMessage(this.state.message)
-    this.setState({
-      message:''
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} name="message" placeholder="Send a message..." value={this.state.message} />
-          <button type="submit">Send</button>
-        </form>
-        <div id="messages">
-          <ul>
-            {this.props.messages.map((message, i)=>(
-              <li key={'message' + i}>{message}</li>
-            ))}
-          </ul>
+  render(){
+    return(
+      <Router>
+        <div>
+        
+          <Route path="/chatRoom/" component={ChatRoom} />
         </div>
-      </div>
-    )
-  }
+      </Router>
+        )
+    }
 }
+export default App
 
-const mapStateToProps = function(appState) {
-  return {
-    messages: appState.messages
-  }
-}
-
-export default connect(mapStateToProps)(App)
+  // <Route exact={true} path="/" component={Login} />
